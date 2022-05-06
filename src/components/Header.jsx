35719ae9d100fcc0dd04a86e946d2561aa4bff29
Franco Coll <francoadrian.coll2012@gmail.com>
2022-05-06 12:43:52 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import NuevoPresupuesto from './NuevoPresupuesto'
+import ControlPresupuesto from './ControlPresupuesto'
+
+const Header = ({ 
+  gastos,
+  setGastos,
+  presupuesto,
+  setPresupuesto,
+  isValidPresupuesto,
+  setIsValidPresupuesto }) => {
+  return (
+    <header>
+      <h1>Control de gastos</h1>
+      {isValidPresupuesto ? (
+        <ControlPresupuesto
+          presupuesto={presupuesto}
+          gastos={gastos}
+          setGastos={setGastos}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      ) : (
+        <NuevoPresupuesto
+          presupuesto={presupuesto}
+          setPresupuesto={setPresupuesto}
+          setIsValidPresupuesto={setIsValidPresupuesto}
+        />
+      )}
+    </header>
+  )
+}
+
+export default Header
